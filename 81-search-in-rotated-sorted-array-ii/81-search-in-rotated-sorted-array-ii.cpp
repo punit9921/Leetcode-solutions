cd3ace186d29cbf0dd8d99cc0cd496fdd6f8c35b
punit9921 <54074690+punit9921@users.noreply.github.com>
2022-03-29 00:33:18 +0530

    Time: 18 ms (8.23%), Space: 13.9 MB (90.12%) - LeetHub

---
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+   
+    bool search(vector<int>& nums, int target) {
+      
+     int low=0,high=nums.size()-1;
+        while(low<=high)
+        {
+            int mid=(low+high)/2;
+            if(nums[mid]==target)
+                return true;
+         if(nums[low]==nums[mid] and nums[mid]==nums[high])
+                low++,high--;
+            else if(nums[low]<=nums[mid])
+            {
+                if(nums[low]<=target and nums[mid]>target)
+                    high=mid-1;
+                else
+                    low=mid+1;
+            }
+            else
+            {
+                if(nums[mid]<target and nums[high]>=target)
+                    low=mid+1;
+                else
+                    high=mid-1;
+            }
+        }
+        return false;
+     
+    }
+};
