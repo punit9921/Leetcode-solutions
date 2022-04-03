cca9f9e191222482dc238f04c880d95d5d36d6f0
punit9921 <54074690+punit9921@users.noreply.github.com>
2022-04-04 00:49:18 +0530

    Added solution - LeetHub

---
+++ Segregate even and odd nodes in a Link List - GFG/segregate-even-and-odd-nodes-in-a-link-list.cpp
@@ -0,0 +1,98 @@
+// { Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+void printList(Node* node) 
+{ 
+    while (node != NULL) { 
+        cout << node->data <<" "; 
+        node = node->next; 
+    }  
+    cout<<"\n";
+} 
+
+
+ // } Driver Code Ends
+//User function template for C++
+
+/*
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+*/
+class Solution{
+public:
+    Node* divide(int N, Node *head){
+        // code here
+        Node * oddhead=new Node(-1),*evenhead=new Node(-1);
+        Node* odd=oddhead;
+        Node* curr=head;
+        Node* even=evenhead;
+        while(curr!=NULL)
+        {
+            if(curr->data %2)
+            {
+                oddhead->next=curr;
+                curr=curr->next;
+                oddhead=oddhead->next;
+            }
+            else
+            {
+                evenhead->next=curr;
+                curr=curr->next;
+                evenhead=evenhead->next;
+            }
+        }
+        evenhead->next=odd->next;
+        oddhead->next=NULL;
+        head=even->next;
+        return head;
+    }
+};
+
+// { Driver Code Starts.
+
+int main() {
+    //code
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        int data;
+        cin>>data;
+        struct Node *head = new Node(data);
+        struct Node *tail = head;
+        for (int i = 0; i < N-1; ++i)
+        {
+            cin>>data;
+            tail->next = new Node(data);
+            tail = tail->next;
+        }
+        
+        Solution ob;
+        Node *ans = ob.divide(N, head);
+        printList(ans); 
+    }
+    return 0;
+}
+  // } Driver Code Ends
