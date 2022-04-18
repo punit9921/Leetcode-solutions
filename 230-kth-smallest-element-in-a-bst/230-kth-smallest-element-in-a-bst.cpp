c6021059524e264d4861dfe59065c0d46a001025
punit9921 <54074690+punit9921@users.noreply.github.com>
2022-04-19 00:34:16 +0530

    Time: 22 ms (62.89%), Space: 24.1 MB (87.81%) - LeetHub

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.cpp
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.cpp
@@ -11,21 +11,22 @@
  */
 class Solution {
 public:
-   int solve(TreeNode* root,int &k)
+    int ans;
+   void solve(TreeNode* root,int &k)
    {
        if(root==NULL)
-           return 0;
-       int left=solve(root->left,k);
-      if(left)
-          return left;
+           return ;
+      solve(root->left,k);
+      
        k=k-1;
        if(k==0)
-           return root->val;
-       return solve(root->right,k);
+           ans= root->val;
+      solve(root->right,k);
        
          //  return right;
    }
     int kthSmallest(TreeNode* root, int k) {
-       return solve(root,k);
+        solve(root,k);
+        return ans;
     }
 };
