782b2513d0d68eebf2405b2d0ddf8d21d650e1b1
punit9921 <54074690+punit9921@users.noreply.github.com>
2022-05-10 06:59:58 +0530

    Time: 411 ms (40.70%), Space: 109.1 MB (9.39%) - LeetHub

--- 1499-max-value-of-equation/1499-max-value-of-equation.cpp
+++ 1499-max-value-of-equation/1499-max-value-of-equation.cpp
@@ -1,24 +1,44 @@
 class Solution {
 public:
     int findMaxValueOfEquation(vector<vector<int>>& points, int k) {
-        deque<vector<int>>q;
-        q.push_back({points[0][0],points[0][1]-points[0][0]});
-            int ans=INT_MIN;
-           for(int i=1;i<points.size();i++)
-            {
-                while(!q.empty() and points[i][0]-q.front()[0]>k)
-                    q.pop_front();
-               if(!q.empty())
-               {
-                   ans=max(ans,q.front()[1]+points[i][0]+points[i][1]);
-               }
-               int val=points[i][1]-points[i][0];
-               while(!q.empty() and q.back()[1]<=val)
-                   q.pop_back();
-               q.push_back({points[i][0],val});
+//         deque<vector<int>>q;
+//         q.push_back({points[0][0],points[0][1]-points[0][0]});
+//             int ans=INT_MIN;
+//            for(int i=1;i<points.size();i++)
+//             {
+//                 while(!q.empty() and points[i][0]-q.front()[0]>k)
+//                     q.pop_front();
+//                if(!q.empty())
+//                {
+//                    ans=max(ans,q.front()[1]+points[i][0]+points[i][1]);
+//                }
+//                int val=points[i][1]-points[i][0];
+//                while(!q.empty() and q.back()[1]<=val)
+//                    q.pop_back();
+//                q.push_back({points[i][0],val});
                    
                
-            }
+//             }
+//         return ans;
+        
+        
+        deque<vector<int>>q;
+        q.push_back({points[0][0],points[0][1]-points[0][0]});
+        int ans=INT_MIN;
+        for(int i=1;i<points.size();i++)
+        {
+            while(!q.empty() and points[i][0]-q.front()[0]>k)
+                q.pop_front();
+            if(!q.empty())
+                ans=max(ans,points[i][0]+points[i][1]+q.front()[1]);
+            int val=points[i][1]-points[i][0];
+            while(!q.empty() and q.back()[1]<=val)
+                q.pop_back();
+            q.push_back({points[i][0],val});
+        }
         return ans;
     }
 };
+
+// yi+yj+|xi-xj|
+// Yi-xi + yj-xj
