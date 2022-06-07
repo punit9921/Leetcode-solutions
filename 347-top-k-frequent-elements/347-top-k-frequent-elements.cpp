784daa1796c8024a361c6d38d07140867b8a1a34
punit9921 <54074690+punit9921@users.noreply.github.com>
2022-06-07 23:14:16 +0530

    Time: 29 ms (24.38%), Space: 13.8 MB (21.68%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
@@ -1,22 +1,38 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-      vector<int>ans;
-        map<int,int>mp;
-        for(int i=0;i<nums.size();i++)
-            mp[nums[i]]++;
-        priority_queue<pair<int,int>>heap;
-    for(auto itr=mp.begin();itr!=mp.end();itr++)
-    {
-        heap.push({itr->second,itr->first});
+//       vector<int>ans;
+//         map<int,int>mp;
+//         for(int i=0;i<nums.size();i++)
+//             mp[nums[i]]++;
+//         priority_queue<pair<int,int>>heap;
+//     for(auto itr=mp.begin();itr!=mp.end();itr++)
+//     {
+//         heap.push({itr->second,itr->first});
        
+//     }
+//         while(k--)
+//         {
+//             ans.push_back(heap.top().second);
+//             heap.pop();
+//         }
+//         return ans;
+//     }
+    vector<int>ans;
+    map<int,int>freq;
+    for(auto it:nums)
+        freq[it]++;
+    priority_queue<pair<int,int>>pq;
+    for(auto it:freq)
+    {
+        pq.push({it.second,it.first});
     }
-        while(k--)
-        {
-            ans.push_back(heap.top().second);
-            heap.pop();
-        }
-        return ans;
+    while(k--)
+    {
+        ans.push_back(pq.top().second);
+        pq.pop();
+    }
+    return ans;
     }
     
 };
