5622c817c25aae1c197b728fe550cecd8c2c4ba3
punit9921 <54074690+punit9921@users.noreply.github.com>
2022-06-18 18:00:28 +0530

    Time: 14 ms (48.83%), Space: 7.1 MB (61.70%) - LeetHub

---
+++ 686-repeated-string-match/686-repeated-string-match.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int repeatedStringMatch(string a, string b) {
+        string s="";
+        int cnt=0;
+        while(s.size()<b.size())
+        {
+            s+=a;
+            cnt++;
+        }
+        if(s.find(b)!=string::npos)
+            return cnt;
+        cnt++;
+        s+=a;
+        if(s.find(b)!=string::npos)
+            return cnt;
+        return -1;
+    }
+};
