2acddfc454ef906b872178dc99fdf62bafbcdf8c
punit9921 <54074690+punit9921@users.noreply.github.com>
2022-07-11 17:04:24 +0530

    Time: 684 ms (46.38%), Space: 57.9 MB (69.95%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -1,30 +1,66 @@
 class Solution {
 public:
-    int minCostConnectPoints(vector<vector<int>>& points) {
-        int n=points.size();
-        vector<bool>mst(n,false);
-        int min_cost=0;
-        int remaining=0;
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<>>pq;
-        pq.push({0,0});
-        while(remaining<n)
+     int par[1001]; // parent array to store parent of every node
+    
+    // Kruskal uses DSU
+    
+    int find(int a) // find function to find parent of a node
+    {
+        if(par[a] < 0)
+            return a;
+        
+        return par[a] = find(par[a]); // path compression
+    }
+    
+    // Union function to make one node parent of anthor
+    void Union(int a, int b)
+    {
+        par[a] = b; // here we are making parent of a to b
+    }
+    
+    int minCostConnectPoints(vector<vector<int>>& arr) {
+        int n = arr.size(); // extract the size of array
+        
+        // Intially all are alone, so everyone is -1
+        for(int i = 0; i < n; i++) par[i] = -1;
+        
+        // define adj vector to store three things
+        // first value store weight where
+        // weight is actually manhattan distance
+        
+        vector<pair<int, pair<int, int>>> adj;
+        
+        // generating graph basically with weights
+        for(int i = 0; i < n; i++)
         {
-            int wt=pq.top().first;
-            int node=pq.top().second;
-            pq.pop();
-            if(mst[node])
-                continue;
-            mst[node]=true;
-            min_cost+=wt;
-            remaining++;
-            for(int i=0;i<n;i++)
-            {   
-                if(!mst[i])
-                {int next_wt=abs(points[node][0]-points[i][0])+abs(points[node][1]-points[i][1]);
-                 pq.push({next_wt,i});
-                }
+            for(int j = i + 1; j < n; j++)
+            {
+                int weight = abs(arr[i][0] - arr[j][0]) + 
+                             abs(arr[i][1] - arr[j][1]);//manhattan distance
+                
+                adj.push_back({weight, {i, j}});
+                
             }
         }
-        return min_cost;
+        
+        // sort on the basis of their edge weight
+        sort(adj.begin(), adj.end());
+        
+        int sum = 0; //intially define sum as zero
+        
+        // for each edge we travel
+        for(int i = 0; i < adj.size(); i++)
+        {
+            int a = find(adj[i].second.first); // first node
+            int b = find(adj[i].second.second); // second node
+            
+            if(a != b) // if both parents are not same then add weight
+            {
+                sum += adj[i].first;
+                Union(a, b); // now merge them
+            }
+        }
+        
+        return sum; // finally return sum
     }
 };
