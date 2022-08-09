c9230b8177ff0027f91febdc6980d1212cbb0954
punit9921 <54074690+punit9921@users.noreply.github.com>
2022-08-09 10:16:34 +0530

    Time: 31 ms (64.15%), Space: 14 MB (54.56%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>>adj(numCourses);
+        vector<int>indegree(numCourses,0);
+        for(auto it:prerequisites)
+        {
+            adj[it[1]].push_back(it[0]);
+            indegree[it[0]]++;
+            
+        }
+        queue<int>q;
+        for(int i=0;i<numCourses;i++)
+        {
+            if(indegree[i]==0)
+                q.push(i);
+                
+        }
+        int cnt=0;
+        while(!q.empty())
+        {
+            int node=q.front();
+            q.pop();
+            cnt++;
+            for(auto it:adj[node])
+            {
+                if(--indegree[it]==0)
+                {
+                    q.push(it);
+                }
+            }
+        }
+        return cnt==numCourses;
+    }
+};
