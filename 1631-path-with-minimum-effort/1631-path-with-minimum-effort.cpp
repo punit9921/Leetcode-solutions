6a660fe5b3bb9e6f788a6a2ad724cf4f91042476
punit9921 <54074690+punit9921@users.noreply.github.com>
2022-10-20 23:14:25 +0530

    Time: 943 ms (14.95%), Space: 41.6 MB (28.53%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -3,32 +3,33 @@ public:
     int minimumEffortPath(vector<vector<int>>& heights) {
         int n=heights.size();
         int m=heights[0].size();
-        vector<vector<int>>dis(n,vector<int>(m,INT_MAX));
         priority_queue<vector<int>,vector<vector<int>>,greater<>>pq;
         pq.push({0,0,0});
-        dis[0][0]=0;
+        vector<vector<int>>dist(n,vector<int>(m,INT_MAX));
+        dist[0][0]=0;
         int dx[]={-1,1,0,0};
         int dy[]={0,0,-1,1};
+        
         while(!pq.empty())
         {
             auto temp=pq.top();
             pq.pop();
-            int dist=temp[0];
+            int dis=temp[0];
             int x=temp[1];
             int y=temp[2];
             if(x==n-1 and y==m-1)
-                return dist;
+                return dis;
             for(int i=0;i<4;i++)
             {
                 int nx=x+dx[i];
                 int ny=y+dy[i];
                 if(nx<0 or nx>=n or ny<0 or ny>=m)
                     continue;
-                int new_wt=max(dist,abs(heights[nx][ny]-heights[x][y]));
-                if(new_wt<dis[nx][ny])
+                int new_dis=max(dis,abs(heights[nx][ny]-heights[x][y]));
+                if(new_dis<dist[nx][ny])
                 {
-                    dis[nx][ny]=new_wt;
-                    pq.push({dis[nx][ny],nx,ny});
+                    dist[nx][ny]=new_dis;
+                    pq.push({new_dis,nx,ny});
                 }
             }
         }
